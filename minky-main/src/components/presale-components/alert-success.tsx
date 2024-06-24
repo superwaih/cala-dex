5e8bb252f0e5367d8165fceab996cbb1a5ab983c
Victor Abidoye <abidoye8224@gmail.com>
2024-06-24 09:44:57 +0100

    testing

--- minky-main/src/components/presale-components/alert-success.tsx
+++ minky-main/src/components/presale-components/alert-success.tsx
@@ -8,7 +8,7 @@ const SuccessAlert = () => {
     <Terminal className="h-4 w-4" />
     <AlertTitle>Heads up!</AlertTitle>
     <AlertDescription>
-      You can add components and dependencies to your app using the cli.
+      You can add components hhfhand dependencies to your app using the cli.
     </AlertDescription>
   </Alert>
   )
